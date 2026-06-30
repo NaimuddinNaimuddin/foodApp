fd8667fd2055bbee45dcbdbb49af341f34bdb42f
naimuddin <naimuddin5409@gmail.com>
2026-06-30 23:30:55 +0530

    error message fix

--- foodAppBackend/routes/users.js
+++ foodAppBackend/routes/users.js
@@ -29,7 +29,7 @@ router.post("/signup", async (req, res) => {
 
         res.status(201).json({ message: "User registered successfully" });
     } catch (error) {
-        res.status(500).json({ message: "Server error" });
+        res.status(500).json({ message: error });
     }
 });
 
@@ -64,7 +64,7 @@ router.post("/login", async (req, res) => {
             },
         });
     } catch (error) {
-        res.status(500).json({ message: "Server error" });
+        res.status(500).json({ message: error });
     }
 });
 
